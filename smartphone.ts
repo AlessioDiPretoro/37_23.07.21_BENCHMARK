a8f0c90b6ccd2822dbd955e42f0fd4728848c6bc
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-07-21 13:01:44 +0200

    okFilter

--- smartphone.ts
+++ smartphone.ts
@@ -2,43 +2,84 @@ interface Smartphone {
   carica: number; 
   numeroChiamate: number; 
   costoMinuto: number; 
+  registroChiamate:{
+    id:number,
+    durata:number,
+    dataEora:string
+  }[]
   ricarica(euro: number): void;
-  numero404(): string;
-  getNumeroChiamate(): number;
+  get numero404(): string;
+  get getNumeroChiamate(): number;
   chiamata(min: number): void;
   azzeraChiamate(): void;
+  mostraRegistroChiamate():{}[]
+  filtraChiamatePerDataOra(dataDaRicercare:string):{}[]
 }
+let filteredCalls:{}[];
 
-let Smartphone1: Smartphone = {
-  carica: 0,
-  numeroChiamate: 0,
-  costoMinuto: 0.2,
-  ricarica: function (euro: number): void {
+class User implements Smartphone {
+  
+  carica: number
+  numeroChiamate: number
+  costoMinuto: number
+  registroChiamate: {
+    id: number,
+    durata: number,
+    dataEora: string,
+  }[]
+  constructor(_carica:number, _numeroChiamate:number, _costoMinuto:number = 0.20, _registroChiamate:{id: number,
+    durata: number,
+    dataEora: string,}[]){
+    this.carica = _carica
+    this.numeroChiamate = _numeroChiamate
+    this.costoMinuto = _costoMinuto
+    this.registroChiamate = _registroChiamate
+  }
+  ricarica(euro: number): void {
     this.carica += euro;
-  },
-  numero404: function (): string {
+  }
+  get numero404(): string {
     return "Saldo disponibile:" + this.carica + "€";
-  },
-  getNumeroChiamate: function (): number {
+  }
+  get getNumeroChiamate(): number {
     return this.numeroChiamate;
-  },
-  chiamata: function (min: number): void {
+  }
+  chiamata(min: number): void {
     if (min * this.costoMinuto < this.carica) {
-      this.numeroChiamate ++;
+      this.numeroChiamate++;
       this.carica = this.carica - this.costoMinuto * min;
+      let today: string = Date();
+      //let today: string = "Fri Jul 21 2023 12:24:10 GMT+0200 (Ora legale dell’Europa centrale)"; //usato per prove a mano
+      this.registroChiamate.push({ id: this.registroChiamate.length, durata: min, dataEora: today });
     } else throw new Error("Effettuare una ricarica.");
-  },
-  azzeraChiamate: function (): void {
+  }
+  azzeraChiamate(): void {
     this.numeroChiamate = 0;
-  },
+    this.registroChiamate.splice(1,this.registroChiamate.length)
+    // ok cancella tutto tranne il primo elemento di default
+  }
+  mostraRegistroChiamate(): {}[] {
+    return this.registroChiamate;
+  }
+  filtraChiamatePerDataOra(dataDaRicercare: string): {}[] {
+    
+    return this.registroChiamate.filter(item => item.dataEora.includes(dataDaRicercare));
+  
 };
-
-Smartphone1.ricarica(20);
-console.log("Smartphone1 dopo prima ricarica", Smartphone1);
-console.log("numero404",Smartphone1.numero404());
-console.log("getNumeroChiamate",Smartphone1.getNumeroChiamate());
-//Smartphone1.chiamata(1000) //mostra l'errore dovuto al credito residuo non sufficiente
-Smartphone1.chiamata(10)
-console.log("Smartphone1 dopo prima chiamata", Smartphone1);
-Smartphone1.azzeraChiamate()
-console.log("Smartphone1 dopo azzera chiamata", Smartphone1);
+}
+let user1 = new User(20, 0, 0.20, [{id:0, durata:0, dataEora:""}])
+user1.chiamata(1)
+setTimeout(()=> user1.chiamata(1), 1000)
+setTimeout(()=> user1.chiamata(1), 2000)
+setTimeout(()=> user1.chiamata(2), 3000)
+setTimeout(()=> user1.chiamata(3), 4000)
+setTimeout(()=> user1.chiamata(3), 5000)
+setTimeout(()=> user1.chiamata(3), 6000)
+// setTimeout(()=> console.log(user1.mostraRegistroChiamate()), 5000)
+// setTimeout(()=> user1.azzeraChiamate(), 6000) // ok cancella tutto tranne il primo elemento di default
+// console.log("Registro Chiamate:", user1.mostraRegistroChiamate());
+// console.log("chiamate filtrate:", user1.filtraChiamatePerDataOra("12:59"));
+setTimeout(() => console.log(user1.mostraRegistroChiamate()), 7000);
+setTimeout(() => console.log("chiamate filtrate", user1.filtraChiamatePerDataOra("13:01:20")), 7000);
+// user1.azzeraChiamate()
+// console.log(user1.mostraRegistroChiamate());
